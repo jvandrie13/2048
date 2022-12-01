d7fa1628770c9be340670da46c6c00cfefb21c87
Shayla Lunn <slunn16@iastate.edu>
2022-12-01 15:43:15 -0600

    updates

--- app/src/main/java/com/example/a2048/GameOver.java
+++ app/src/main/java/com/example/a2048/GameOver.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class GameOver extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.game_over);
     }
 
-    public void onClickNewGame(View view) {
-        Intent gameIntent = new Intent(MainActivity.this, Game.class);
-        MainActivity.this.startActivity(gameIntent);
+    public void onClickTryAgain(View view) {
+        Intent gameIntent = new Intent(GameOver.this, Game.class);
+        GameOver.this.startActivity(gameIntent);
     }
 }
