bd1024e3f91e74a1250c06903e678ae656d0458d
Van Drie Joshua D <VanDrieJoshuaD@JohnDeere.com>
2022-12-04 16:35:55 -0600

    start of gridlayout

--- app/src/main/java/com/example/a2048/Game.java
+++ app/src/main/java/com/example/a2048/Game.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Game extends AppCompatActivity {
+    private Grid grid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
+        grid = findViewById(R.id.grid);
     }
 
 }
