14de9baf0d01b43f0c29c16244e6366617113d4e
Shayla Lunn <slunn16@iastate.edu>
2022-12-04 16:13:16 -0600

    add grid class

---
+++ app/src/main/java/com/example/a2048/Grid.java
@@ -0,0 +1,13 @@
+package com.example.a2048;
+import android.content.Context;
+import android.widget.GridLayout;
+
+
+public class Grid extends GridLayout {
+
+    public Grid(Context context) {
+        super(context);
+
+    }
+
+}
