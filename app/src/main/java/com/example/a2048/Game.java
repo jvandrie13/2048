521315aa5230f6146e77458e786a07c6a0250854
Van Drie Joshua D <VanDrieJoshuaD@JohnDeere.com>
2022-12-09 12:04:02 -0600

    make random tiles appear

--- app/src/main/java/com/example/a2048/Game.java
+++ app/src/main/java/com/example/a2048/Game.java
@@ -2,10 +2,17 @@ package com.example.a2048;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 public class Game extends AppCompatActivity {
     private Grid grid;
+    Button newGame;
 
 
     @Override
@@ -13,6 +20,17 @@ public class Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
         grid = findViewById(R.id.grid);
+        newGame = findViewById(R.id.newGame);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+        newGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                grid.onSizeChanged(1125, 1537, 0 ,0);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/a2048/Grid.java
+++ app/src/main/java/com/example/a2048/Grid.java
@@ -1,15 +1,21 @@
 package com.example.a2048;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.GridLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Grid extends GridLayout {
 
     public static Card[][] cards = new Card[4][4];
+    private static List<Point> emptyPoints = new ArrayList<Point>();
+
 
     public Grid(Context context) {
         super(context);
@@ -26,7 +32,7 @@ public class Grid extends GridLayout {
         initGrid();
     }
 
-    private void initGrid() {
+    protected void initGrid() {
         setRowCount(4);
         setColumnCount(4);
         setOnTouchListener(new Listener());
@@ -38,16 +44,10 @@ public class Grid extends GridLayout {
         super.onSizeChanged(w, h, oldw, oldh);
         int cardWidth = (Math.min(w, h)-10)/4;
         addCards(cardWidth, cardWidth);
-//        startGame();
-        for(int y=0;y<4;++y) {
-            for(int x=0;x<4;++x) {
-                cards[x][y].setCount(0);
-            }
-        }
-
+        startGame();
     }
 
-    private void addCards(int cardWidth, int cardHeight) {
+    protected void addCards(int cardWidth, int cardHeight) {
         this.removeAllViews();
         Card c;
         for(int col=0;col<4;col++) {
@@ -58,6 +58,29 @@ public class Grid extends GridLayout {
                 cards[row][col] = c;
             }
         }
+    }
+
+    private static void addRandomNum() {
+        emptyPoints.clear();
+        for (int y = 0; y < 4; ++y) {
+            for (int x = 0; x < 4; ++x) {
+                if (cards[x][y].getCount() == 0) {
+                    emptyPoints.add(new Point(x, y));
+                }
+            }
+        }
+        Point p = emptyPoints.remove((int)(Math.random()*emptyPoints.size()));
+        cards[p.x][p.y].setCount(Math.random()>0.1?2:4);
+    }
+
+    public static void startGame() {
+        for(int y=0;y<4;++y) {
+            for(int x=0;x<4;++x) {
+                cards[x][y].setCount(0);
+            }
+        }
+        addRandomNum();
+        addRandomNum();
     }
 
 
